f23e8c72e2e8d0724a7a9e4c7c4cbc31ce620459
levwtech <abdelraahmanmostafa@gmail.com>
2023-09-21 16:10:44 +0300

    fix: add member login controller

--- route/member.js
+++ route/member.js
@@ -4,5 +4,6 @@ const controller = require('../controller/member');
 const router = new express.Router();
 
 router.post('/register', controller.registerMember);
+router.post('/login', controller.login);
 
 module.exports = router;
