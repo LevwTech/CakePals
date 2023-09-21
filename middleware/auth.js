9633b5b7bc9befb25b95247a23a73161c493bfa2
levwtech <abdelraahmanmostafa@gmail.com>
2023-09-21 18:38:33 +0300

    fix: jwt secret added

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const Baker = require('../model/baker');
 const bakerAuth = async function (req, res, next) {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.send('Please authenticate');
-  const tokenObj = jwt.verify(token, 'authtoken');
+  const tokenObj = jwt.verify(token, process.env.JWT_SECRET);
   const baker = await Baker.findOne({
     _id: tokenObj._id,
     'token.token': tokenObj._id,
@@ -19,7 +19,7 @@ const bakerAuth = async function (req, res, next) {
 const memberAuth = async function (req, res, next) {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.send('Please authenticate');
-  const tokenObj = jwt.verify(token, 'authtoken');
+  const tokenObj = jwt.verify(token, process.env.JWT_SECRET);
   const member = await Member.findOne({
     _id: tokenObj._id,
     'token.token': tokenObj._id,
